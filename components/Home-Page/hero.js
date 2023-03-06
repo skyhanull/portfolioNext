98ac168279e72800ddae9fbacf823b232df094c9
skyhanull <anky0425@naver.com>
2023-03-06 13:40:35 +0900

    feat:hero페이지 생성

---
+++ components/Home-Page/hero.js
@@ -0,0 +1,21 @@
+import classes from "./hero.module.css";
+import Image from "next/image";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/max.png"
+          alt="An image showing Max"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, Max</h1>
+      <p>I blog about web development</p>
+    </section>
+  );
+}
+
+export default Hero;
